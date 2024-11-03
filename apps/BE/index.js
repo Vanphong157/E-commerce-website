08617e82229ce480ed9d488e6a0aadf14c329d8b
VanPhong <vanphong150703@gmail.com>
2024-11-04 03:07:13 +0700

    product api

--- apps/BE/index.js
+++ apps/BE/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const cors = require("cors");
 const dotenv = require("dotenv");
+const productRoutes = require("./routes/productRoute");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error));
